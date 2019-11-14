6bffc2ca26371fc0f04b20bf0333715d21e8fb4e
Andi1603 <andreas.schlager28@gmail.com>
2019-11-14 19:36:18 +0100

    change return type of read_from

--- include/HW_Connector.h
+++ include/HW_Connector.h
@@ -9,7 +9,7 @@ public:
 
     virtual bool device_available() const = 0;
     virtual void write_to(uint8_t register_add, uint8_t value) = 0;
-    virtual void read_from(uint8_t register_add) = 0;
+    virtual uint8_t read_from(uint8_t register_add) = 0;
 };
 
 #endif
