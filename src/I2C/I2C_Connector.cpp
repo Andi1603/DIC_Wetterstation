23bd9dd4c2afc74566330c0725a6b98e31ce49c3
Andi1603 <andreas.schlager28@gmail.com>
2019-11-18 18:09:23 +0100

    implement connector

--- src/I2C/I2C_Connector.cpp
+++ src/I2C/I2C_Connector.cpp
@@ -1,9 +1,8 @@
 
-#include "I2C_Connector.h"
+#include "I2C/I2C_Connector.h"
 
 I2C_Connector::I2C_Connector(uint8_t bus_number, uint8_t device_add) 
-    : m_device_add(device_add) {
-    
+    : m_device_add(device_add), m_i2c_helper(I2C_Helper::get(bus_number)) {
 }
 
 bool I2C_Connector::device_available() const {
@@ -11,9 +10,11 @@ bool I2C_Connector::device_available() const {
 }
 
 void I2C_Connector::write_to(uint8_t register_add, uint8_t value) {
-
+    m_i2c_helper->writeTo(m_device_add, register_add, value);
 }
 
 uint8_t I2C_Connector::read_from(uint8_t register_add) {
-    return 0;
+    uint8_t val = 0;
+    m_i2c_helper->readFrom(m_device_add, register_add, &val, 1);
+    return val;
 }
