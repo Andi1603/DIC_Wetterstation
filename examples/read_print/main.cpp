4658fc3e2d45a21950fbe70c0fbfe0f2df85921f
Andi1603 <andreas.schlager28@gmail.com>
2019-11-18 18:19:09 +0100

    Add read_print example

---
+++ examples/read_print/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "I2C/I2C_Connector.h"
+#include "Sensor/BMP180.h"
+
+BMP180* sensor;
+
+void setup() {
+    Serial.begin(9600);
+
+    HW_Connector* connector = new I2C_Connector(0, BMP180_DEVICE_ADD);
+    sensor = new BMP180(*connector);
+    Serial.println("Temperature [°C]\t|\tPressure [Pa]\t|\tAltitude [m]");
+}
+
+void loop() {
+    Serial.printf("%.1f\t\t\t|\t%ld\t\t|\t%.2f\n", sensor->getTemperature(), sensor->getPressure(BMP180_Resolution::ULTRA_HIGH_RESOLUTION), sensor->getAltitude(BMP180_Resolution::ULTRA_HIGH_RESOLUTION));
+    delay(1000);
+}
